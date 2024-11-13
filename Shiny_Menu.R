9f23df65f16e39c0a14f68752a8266423ac48cd8
gmw12 <greg.waitt@duke.edu>
2024-11-13 13:03:24 -0500

    fix

--- Shiny_Menu.R
+++ Shiny_Menu.R
@@ -23,8 +23,8 @@ load_menu <- function(session, input, output) {
       load_menu_precursor_to_protein(session, input, output)
     }else if (params$raw_data_format == "precursor" & params$data_output == "Peptide") {
       load_menu_peptide(session, input, output)
-    }else if (raw_data_format == "protein") {
-      load_menu_all(session, input, output)
+    }else if (params$raw_data_format == "protein") {
+      load_menu_protein(session, input, output)
     }
   }
 
@@ -107,6 +107,68 @@ load_menu_precursor_to_protein <- function(session, input, output) {
     )
   })
   
+  output$menu_admin <- renderMenu({ 
+    menuItem("Admin", tabName = "admin")
+  })
+  
+}
+
+#----------------------------------------------------------------------------------------- 
+load_menu_protein <- function(session, input, output) {
+  
+  output$menu_load <- renderMenu({ 
+    menuItem("Load", tabName = "load")
+  })
+  
+  output$menu_parameters <- renderMenu({ 
+    menuItem("Parameters", tabName = "parameters")
+  })
+  
+  output$menu_noise <- renderMenu({ 
+    menuItem("Noise", tabName = "noise")
+  })
+  
+  output$menu_filter <- renderMenu({ 
+    menuItem("Filter", tabName = "filter")
+  })
+  
+  output$menu_normalize <- renderMenu({ 
+    menuItem("Normalize", tabName = "normalize")
+  })
+  
+  output$menu_impute <- renderMenu({ 
+    menuItem("Impute", tabName = "impute")
+  })
+  
+  output$menu_rollup <- renderMenu({ 
+    menuItem("Rollup", tabName = "rollup")
+  })
+  
+  output$menu_qc <- renderMenu({ 
+    menuItem("QC", tabName = "qc")
+  })
+  
+  output$menu_stats <- renderMenu({ 
+    menuItem("Stats", tabName = "stats", startExpanded = FALSE,
+             menuItem("Setup", tabName = "stats_setup"),
+             menuItem("Comparisons", tabName = "stats_compare"),
+             menuItem("Graphs", tabName = "stats_plots"),
+             menuItem("Data", tabName = "stats_data"),
+             menuItem("Protein Plots", tabName = "stats_protein_plots"))
+  })
+  
+  output$menu_pathway <- renderMenu({ 
+    menuItem("Pathway", tabName = "pathway", startExpanded = FALSE,
+             menuItem("Setup", tabName = "pathway_setup"),
+             menuItem("Wiki Pathways", tabName = "pathway_wiki"),
+             menuItem("Go Profile", tabName = "pathway_go_profile"),
+             menuItem("Go Analysis", tabName = "pathway_go_analysis"),
+             menuItem("Go Volcano", tabName = "pathway_go_volcano"),
+             menuItem("StringDB", tabName = "pathway_string"),
+             menuItem("StringDB Enrich", tabName = "pathway_string_enrich")
+    )
+  })
+
   output$menu_admin <- renderMenu({ 
     menuItem("Admin", tabName = "admin")
   })
